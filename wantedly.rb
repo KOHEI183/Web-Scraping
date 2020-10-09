c832d5f2fb021f524800218864b0891456b3bc9f
KOHEI183 <zbtbfuufp@gmail.com>
2020-10-09 19:39:48 +0900

    [add]

--- wantedly.rb
+++ wantedly.rb
@@ -1,52 +1,68 @@
-#TODO:日本語化する
-#TODO:URLをうまく吐き出せない
-#TODO:きれいな配列で抽出できない
+# TODO:値が回り上書きされないindex番号をしているから?
 require "open-uri"
 require "nokogiri"
 require 'csv'
 require 'kconv'
 require 'pry'
 
-CSV.open("list.csv", "w") do |csv|
-  for i in 1..3 do
+for i in 1..3 do
+  count = 0
+  lists = []
     url = "https://www.wantedly.com/projects?type=mixed&page=#{i}&occupation_types%5B%5D=jp__engineering&hiring_types%5B%5D=mid_career&locations%5B%5D=tokyo"
-    begin
-      _html = open(url)
-    rescue OpenURI::HTTPError
-      sleep 1
-      next
+    charset = nil
+    html = OpenURI.open_uri(url) do |f|
+      charset = f.charset
+      f.read
     end
-    doc = Nokogiri::HTML(_html)
-    counts =  doc.css('.project-tags .entry-count .count').inner_text
-    companys =  doc.css('h3').inner_text
-    titles = doc.css('h1').inner_text
-    csv << [counts,companys, titles]
-    sleep 1
+
+    doc = Nokogiri::HTML.parse(html.toutf8, nil, 'utf-8')
+    posts_list = doc.css(".column-main")
+    posts_list.each do |post|
+      data = []
+      company = post.css(".projects-index-single .project-bottom .company-name h3")[0].inner_text
+      title = post.css(".projects-index-single .project-title")[0].inner_text
+      link = post.css(".projects-index-single .project-title a")[0][:href]
+
+      data.push(company.tosjis,title.tosjis,link.tosjis)
+      lists.push(data)
+    end
+
+  CSV.open("wantedly.csv", "w") do |csv|
+    lists.each do |r|
+      csv << r
+    end
   end
 end
 
-#========解説不要なら削除========
-# webに接続するためのライブラリ
-# require "open-uri"
-# クレイピングに使用するライブラリ
-# require "nokogiri"
-# CSV吐き出しに使用するライブラリ
-# require 'csv'
-# CSV.open("list.csv", "w") do |csv|
-#   for i in 1..3 do ←　読み込みたいページ数を入れる。例は1～3ページ。37行目の#{i}にページ数が入り繰り返されるよ！
-#     url = "https://www.wantedly.com/projects?type=mixed&page=#{i}&occupation_types%5B%5D=jp__engineering&hiring_types%5B%5D=mid_career&locations%5B%5D=tokyo"　←　読み込みたいURL入れる複数ページ読みたい場合#{i}に変える
-#     begin
-#       _html = open(url)
-#     rescue OpenURI::HTTPError
-#       sleep 1
-#       next
+
+#========================================
+# for i in 1..3 do
+#   count = 0
+#   lists = []
+#     url = "https://www.wantedly.com/projects?type=mixed&page=#{i}&occupation_types%5B%5D=jp__engineering&hiring_types%5B%5D=mid_career&locations%5B%5D=tokyo"
+#     charset = nil
+#     html = OpenURI.open_uri(url) do |f|
+#       charset = f.charset
+#       f.read # htmlを読み込んで変数htmlに渡す
 #     end
-#     doc = Nokogiri::HTML(_html)
-#     counts =  doc.css('.project-tags .entry-count .count').inner_text　←（）内は読み込みたいクラス名を入れる
-#     companys =  doc.css('h3').inner_text　←（）内は読み込みたいクラス名を入れる
-#     titles = doc.css('h1').inner_text　←（）内は読み込みたいクラス名を入れる
-#     csv << [counts,companys, titles]
-#     sleep 1
+
+# htmlを解析してオブジェクトを生成
+#     doc = Nokogiri::HTML.parse(html.toutf8, nil, 'utf-8')
+#     posts_list = doc.css(".column-main")
+#     posts_list.each do |post|
+#       data = []
+#       company = post.css(".projects-index-single .project-bottom .company-name h3")[0].inner_text
+#       title = post.css(".projects-index-single .project-title")[0].inner_text
+#       link = post.css(".projects-index-single .project-title a")[0][:href]
+
+# data配列に取得した情報を格納
+#       data.push(company.tosjis,title.tosjis,link.tosjis)
+#       lists.push(data)
+#     end
+
+#   CSV.open("wantedly.csv", "w") do |csv|
+#     lists.each do |r|
+#       csv << r
+#     end
 #   end
 # end
-
